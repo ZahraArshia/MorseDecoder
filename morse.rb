c2e8de9792e75c77fb243a16543b3341ebba4df3
Zahra Arshia <fzarshia@gmail.com>
2022-05-12 14:31:39 +0430

    Update morse.rb
    linters fixed

--- morse.rb
+++ morse.rb
@@ -1,4 +1,4 @@
-def charList(char)
+def char_list(char)
   characters = {
     '.-' => 'A',
     '-...' => 'B',
@@ -26,18 +26,18 @@ def charList(char)
     '-..-' => 'X',
     '-.--' => 'Y',
     '--..' => 'Z'
-}
-characters[char]
+  }
+  characters[char]
 end
 
 def decode_word(word)
-  words = word.split(' ')
-  words.map { |letter| charList(letter) }.join('')
+  words = word.split()
+  words.map { |letter| char_list(letter) }.join()
 end
 
 def decode_sentense(sentense)
   sentense = sentense.split('   ')
-  sentense.map {|word| decode_word(word)}.join(' ')
+  sentense.map { |word| decode_word(word) }.join(' ')
 end
 
 decode_sentense('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
